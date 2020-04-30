37dc055f7e74635b7ad9d35f4279ff5ea015f4e7
MediaMorphose <contact@mediamorphose.com>
2020-04-30 13:08:05 +0200

    Updating index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ require_once 'vendor/autoload.php';
 $client = new SapitiClient('test','test');
 $client->setTestMode(true);
 echo $client->System()->ping();
-//echo $client->getLastApiResponse()->getEnvironment()."\n";
+echo $client->getLastApiResponse()->getEnvironment()."\n";
 //echo $client->getLastApiResponse()->getDateTime()->format('Y-m-d H:i:s')."\n";
 //var_dump($client->System()->pingParams([1=>"youpi","prout"=>"hdg jhgj dsgh j"]));

e29e30e99ed0f338d71e492d256d3006b37ec64a
MediaMorphose <contact@mediamorphose.com>
2020-05-04 11:29:31 +0200

    CURLOPT_SSL_VERIFYHOST enabled for production server only

--- src/SapitiClient.php
+++ src/SapitiClient.php
@@ -88,7 +88,7 @@ class SapitiClient
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 
-		if($this->testMode || true) {
+		if($this->testMode) {
 			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		}

deccfba862ac3c0397325abfd4c592b59b9c0741
MediaMorphose <public@mediamorphose.com>
2022-11-15 13:15:49 +0100

    Update Repository.php

--- src/Repositories/Repository.php
+++ src/Repositories/Repository.php
@@ -53,7 +53,7 @@ class Repository
 
 			/** @var ApiResponse $cacheInfo */
 			$cacheInfo = $cacheItem->get();
-			if (!$cacheItem->isMiss() && $cacheInfo) {
+			if ($cacheInfo) {
 				$cacheInfo->setCached(true);
 				$this->lastAPIResponse = $cacheInfo;
 				return $cacheInfo;
